e60f23b667f821d0bd7229907cf7c15741b850a9
juanjosebernalv <juanjosebernalvillamarin@gmail.com>
2024-07-29 21:49:31 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,3 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the comments.html file from the previous exercise.
+// The comments.html file should be served as a response to the GET request to the /comments URL.
